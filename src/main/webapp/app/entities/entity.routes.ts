9fbf31660a1a4d5d78a5cf20d690c2cf3f4b4339
Dragos Chisca <dragoschisca@gmail.com>
2026-03-12 22:44:23 +0200

    ADD:minor changes

--- src/main/webapp/app/entities/entity.routes.ts
+++ src/main/webapp/app/entities/entity.routes.ts
@@ -6,6 +6,36 @@ const routes: Routes = [
     data: { pageTitle: 'libraryApp.adminAuthority.home.title' },
     loadChildren: () => import('./admin/authority/authority.routes'),
   },
+  {
+    path: 'author',
+    data: { pageTitle: 'libraryApp.author.home.title' },
+    loadChildren: () => import('./author/author.routes'),
+  },
+  {
+    path: 'publisher',
+    data: { pageTitle: 'libraryApp.publisher.home.title' },
+    loadChildren: () => import('./publisher/publisher.routes'),
+  },
+  {
+    path: 'book',
+    data: { pageTitle: 'libraryApp.book.home.title' },
+    loadChildren: () => import('./book/book.routes'),
+  },
+  {
+    path: 'client',
+    data: { pageTitle: 'libraryApp.client.home.title' },
+    loadChildren: () => import('./client/client.routes'),
+  },
+  {
+    path: 'borrowed-book',
+    data: { pageTitle: 'libraryApp.borrowedBook.home.title' },
+    loadChildren: () => import('./borrowed-book/borrowed-book.routes'),
+  },
+  {
+    path: 'book-author',
+    data: { pageTitle: 'libraryApp.bookAuthor.home.title' },
+    loadChildren: () => import('./book-author/book-author.routes'),
+  },
   /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
 ];
 
